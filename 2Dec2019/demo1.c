5c3c279a11520a4bec7268f381bad32f87ab2352
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2019-12-02 20:33:44 +0530

    Add files via upload

---
+++ 2Dec2019/demo1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int a[3][2];
+    int i,j;
+
+    printf("Enter the elements\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<2;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Array Elements are \n");
+    for(i=0;i<3;i++){
+        for(j=0;j<2;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
